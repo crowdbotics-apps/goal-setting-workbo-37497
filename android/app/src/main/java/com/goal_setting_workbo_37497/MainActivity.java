4ee1bcebf165ada401074fa827df792caf446245
Crowdbotics <team@crowdbotics.com>
2022-11-04 19:14:09 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/goal_setting_workbo_37497/MainActivity.java
@@ -0,0 +1,15 @@
+package com.goal_setting_workbo_37497;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "goal_setting_workbo_37497";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "goal_setting_workbo_37497",
+  url: "https://goal_setting_workbo_37497.botics.co",
+  api: "https://goal_setting_workbo_37497.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
